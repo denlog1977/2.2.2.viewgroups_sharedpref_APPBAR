5b03059d88607826cc3ac907ce06dfd7368538f5
denlog1977 <denlog1977@gmail.com>
2019-10-27 21:25:53 +0300

    Initial commit

---
+++ app/src/main/java/com/example/a222viewgroups_sharedpref_appbar/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.a222viewgroups_sharedpref_appbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.action_open_notes) {
+
+            Toast.makeText(MainActivity.this, "Отркыть записную книжку", Toast.LENGTH_LONG).show();
+            Intent intentNotes = new Intent(MainActivity.this, NotesActivity.class);
+            startActivity(intentNotes);
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
